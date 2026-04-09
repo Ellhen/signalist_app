ab4eec4924e4a3ff04bb4f45f514f59f7c2b8dbe
Ella <ella.tseluiko@gmail.com>
2026-04-09 15:17:25 -0500

    Implement stock details page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,14 @@
 import TradingViewWidget from '@/components/TradingViewWidget'
-import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from '@/lib/constants'
+import {
+  HEATMAP_WIDGET_CONFIG,
+  MARKET_DATA_WIDGET_CONFIG,
+  MARKET_OVERVIEW_WIDGET_CONFIG,
+  TOP_STORIES_WIDGET_CONFIG
+} from '@/lib/constants'
 
 const Home = () => {
   const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`
+
   return (
     <div className="flex min-h-screen home-wrapper">
       <section className="grid w-full gap-8 home-section">
@@ -24,12 +30,11 @@ const Home = () => {
           />
         </div>
       </section>
-        <section className="grid w-full gap-8 home-section">
+      <section className="grid w-full gap-8 home-section">
         <div className="h-full md:col-span-1 xl:col-span-1">
           <TradingViewWidget
             scriptUrl={`${scriptUrl}timeline.js`}
             config={TOP_STORIES_WIDGET_CONFIG}
-            className="custom-chart"
             height={600}
           />
         </div>
